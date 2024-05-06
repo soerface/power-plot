9859c12e1647e66ca0d2adb2f990851ad4a88bb3
Soeren Wegener <wegener92@gmail.com>
2024-05-06 21:26:32 +0200

    Better visualization of the phases. Store current day from csv

--- main.py
+++ main.py
@@ -27,16 +27,15 @@ def download_data():
     # Split to one dataframe per day
     df["Date"] = df["Date/time UTC"].dt.date
     for date in tqdm(df["Date"].unique(), desc="Saving data"):
-        # Do not save data of the current day, as it is not complete yet
-        if date == pd.Timestamp.now().date():
-            tqdm.write(f"Skipping {date} as it is the current day")
-            continue
         day_df = df[df["Date"] == date]
-        year, month = date.year, date.month
-        file_path = CSV_PATH / f"{year}/{month:02}/{date}.csv"
-        if file_path.exists():
-            tqdm.write(f"Skipping {date} as it already exists")
-            continue
+        if date == pd.Timestamp.now().date():
+            file_path = CSV_PATH / f"current_day.csv"
+        else:
+            year, month = date.year, date.month
+            file_path = CSV_PATH / f"{year}/{month:02}/{date}.csv"
+            if file_path.exists():
+                tqdm.write(f"Skipping {date} as it already exists")
+                continue
         file_path.parent.mkdir(parents=True, exist_ok=True)
         day_df.to_csv(file_path, index=False, columns=[
             "Date/time UTC",
@@ -51,7 +50,8 @@ def main(
         sample_rate: str | None = None,
         plot_phases=False,
         timeframe_start: str | None = None,
-        timeframe_end: str | None = None
+        timeframe_end: str | None = None,
+        y_axis_limit: int | None = None,
 ):
     if download:
         download_data()
@@ -68,10 +68,13 @@ def main(
     files = list(CSV_PATH.rglob("*.csv"))
     dfs = []
     for file in sorted(files):
-        if timeframe_start and timeframe_start.date() > pd.Timestamp(file.stem).date():
-            continue
-        if timeframe_end and timeframe_end.date() < pd.Timestamp(file.stem).date():
-            continue
+        if (timeframe_start or timeframe_end) and file.stem != "current_day":
+            file_date = pd.Timestamp(file.stem).date()
+            if timeframe_start and timeframe_start.date() > file_date:
+                continue
+            if timeframe_end and timeframe_end.date() < file_date:
+                continue
+        print(f"Reading {file}")
         df = pd.read_csv(file, parse_dates=["Date/time UTC"])
         dfs.append(df)
     df = pd.concat(dfs)
@@ -95,23 +98,36 @@ def main(
 
     # Plot the data
     if plot_phases:
-        y_columns = [
-            "Total Active energy Wh",
-            "Active energy Wh (A)", "Active energy Wh (B)", "Active energy Wh (C)",
-            "Total Returned energy Wh",
-            "Returned energy Wh (A)", "Returned energy Wh (B)", "Returned energy Wh (C)",
-        ]
-    else:
-        y_columns = ["Total Active energy Wh", "Total Returned energy Wh"]
-    ts.plot(y=y_columns,
-            title="Total Active and Returned energy Wh",
+        ax = ts.plot.area(
+            y=["Active energy Wh (A)", "Active energy Wh (B)", "Active energy Wh (C)"],
+            title="Active energy Wh per phase",
             xlabel="Date/time UTC",
             ylabel="Energy Wh",
             figsize=(20, 10),
             grid=True,
-            style=["-", ":", "--", "-.", "-", ":", "--", "-."],
-            drawstyle="steps-post",
-            )
+            ylim=(0, y_axis_limit) if y_axis_limit else None,
+        )
+        ts.plot.area(
+            ax=ax,
+            y=["Returned energy Wh (A)", "Returned energy Wh (B)", "Returned energy Wh (C)"],
+            title="Returned energy Wh per phase",
+            xlabel="Date/time UTC",
+            ylabel="Energy Wh",
+            figsize=(20, 10),
+            grid=True,
+            ylim=(0, y_axis_limit) if y_axis_limit else None,
+        )
+    else:
+        ts.plot(y=["Total Active energy Wh", "Total Returned energy Wh"],
+                title="Total Active and Returned energy Wh",
+                xlabel="Date/time UTC",
+                ylabel="Energy Wh",
+                figsize=(20, 10),
+                grid=True,
+                style=["-", ":", "--", "-.", "-", ":", "--", "-."],
+                drawstyle="steps-post",
+                ylim=(0, y_axis_limit) if y_axis_limit else None,
+                )
 
     # Save the plot
     plt.savefig("data/plot.png")
@@ -127,5 +143,6 @@ if __name__ == "__main__":
     args.add_argument("--plot-phases", action="store_true", help="Plot the data for each phase")
     args.add_argument("--timeframe-start", type=str, help="Plot the graph starting at this datetime")
     args.add_argument("--timeframe-end", type=str, help="Plot the graph ending at this datetime")
+    args.add_argument("--y-axis-limit", type=int, help="Limit the y-axis to this value")
     args = args.parse_args()
-    main(args.download, args.sample_rate, args.plot_phases, args.timeframe_start, args.timeframe_end)
+    main(args.download, args.sample_rate, args.plot_phases, args.timeframe_start, args.timeframe_end, args.y_axis_limit)
